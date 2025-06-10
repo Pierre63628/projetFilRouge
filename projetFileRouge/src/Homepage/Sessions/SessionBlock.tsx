aa87c2f7c49047e1cc4444620ed167b54789cd55
Sonia2 <ksarlary@gmail.com>
2025-06-10 16:42:48 +0200

    add imgs

--- projetFileRouge/src/Homepage/Sessions/SessionBlock.tsx
+++ projetFileRouge/src/Homepage/Sessions/SessionBlock.tsx
@@ -3,9 +3,9 @@ import type { Session } from "../Homepage"
 function SessionBlock(session: Session){
     return(
         <div>
-            <img src={session.image} alt="#"/>
+            <p className="sessionName">{session.name}</p>
             <div>
-                <p className="sessionName">{session.name}</p>
+                <img src={session.image} alt="#"/>
                 <p className="sessionTheme">{session.theme}</p>
                 <p className="sessionPlayerNb">{session.playerNb} players</p>
                 <p className="sessionDifficulty">Difficulty: {session.difficulty}</p>
